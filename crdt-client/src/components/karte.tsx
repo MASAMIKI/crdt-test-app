57f7100c0d0071eb283a3e731e7566a51120181b
MASAMIKI <masamiki.matsubara@amd-lab.com>
2023-08-19 17:14:06 +0900

    Yjsへの置き替え

--- crdt-client/src/components/karte.tsx
+++ crdt-client/src/components/karte.tsx
@@ -1,115 +1,77 @@
 'use client'
 
+import * as Y from 'yjs'
+import { WebsocketProvider } from 'y-websocket'
+
 import {useEffect, useState} from "react";
-import {ToastContainer, toast} from "react-toastify";
 
 import 'react-toastify/dist/ReactToastify.css';
 
-type SendMessage = {
-  key: string;
-  sentBy: string;
-  content: string;
-}
-
-type ReceiveMessage = {
-  key: string;
-  sentBy: string;
-  content: string;
-}
-
 const Karte = () => {
   const projectId = "01H7ZD6V8S7XE3A2HHRD6J0TSG" // 仮のプロジェクトID
-  const [ws, setWs] = useState<WebSocket>(null);
+  const [wsProvider, setWsProvider] = useState<WebsocketProvider>(null);
+  const [doc, setDoc] = useState<Y.Doc>(null);
+
   const [isConnected, setIsConnected] = useState(false);
-  const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
   const [userId, setUserId] = useState('');
 
+  const [yText, setYText] = useState<Y.Text>(null);
+
   useEffect(() => {
+    const doc = new Y.Doc()
+    setDoc(doc);
+
     return () => {
-      if (ws) ws.close();
+      if (wsProvider) wsProvider.destroy();
     };
   }, []);
 
-  const onOpen = () => {
-    setIsConnected(true);
-    toast(`WebSocket connected!`)
-  }
-
-  const onClose = () => {
-    toast(isConnected ? `WebSocket disconnected!` : 'WebSocket connection failed!')
-    setIsConnected(false);
-  }
-
-  const onError = (event: Event) => {
-    console.error(event);
-  }
-
-  const onMessage = (event: MessageEvent) => {
-    event.data.text().then((text) => {
-      const data: ReceiveMessage[] = JSON.parse(text);
-      setMessages(prevMessages => [...prevMessages, ...data.map((datum) => `${datum.sentBy}:${datum.content}`)]);
-    }).catch((error) => {
-      console.error(error);
-    })
-  }
 
   const joinRoom = () => {
     // WebSocketの接続を確立
-    const websocket = new WebSocket(`ws://localhost:8080/project/${projectId}/karte?userId=${userId}`);
-    websocket.onopen = onOpen;
-    websocket.onclose = onClose
-    websocket.onerror = onError
-    websocket.onmessage = onMessage
-    setWs(websocket);
+    const wsProvider = new WebsocketProvider(
+`ws://localhost:8080/project/karte`,
+      projectId,
+      doc
+    )
+    wsProvider.on('status', event => {
+      if (event.status === 'connected') {
+        setIsConnected(true);
+      } else {
+        setIsConnected(false);
+      }
+    })
+    setWsProvider(wsProvider);
+
+    // Yjsのテキストタイプを作成
+    const yText = doc.getText('message');
+    yText.observe(event => {
+      // yTextが変更されるたびに、このコードが実行されます
+      const updatedContent = yText.toString();
+      setInput(updatedContent)
+    });
+
+    setYText(yText)
   }
 
   const leaveRoom = () => {
-    ws.close();
-    setWs(null);
-    setMessages([])
+    if (wsProvider) wsProvider.destroy();
+    setWsProvider(null);
+    setInput('');
   }
 
-  const handleSend = () => {
-    if (ws) {
-      const data: SendMessage = {
-        key: "message",
-        content: input,
-        sentBy: userId,
-      }
-      const text = JSON.stringify(data);
-      const binaryData = new TextEncoder().encode(text);
-      ws.send(binaryData);
-      setInput(''); // 入力フィールドをクリア
-    }
+  const handleInputChange = (e) => {
+    const newValue = e.target.value;
+    setInput(newValue);
+
+    yText.delete(0, yText.toString().length);
+    yText.insert(0, newValue);
   };
 
   return (
     <>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
       <p>状態: {isConnected ? "接続中" : "切断中"}</p>
-      <div className="w-1/3">
-        <label htmlFor="messages" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Messages (readOnly)
-        </label>
-        <textarea id="messages" rows="4"
-                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  value={messages.join('\n')}
-                  readOnly
-        >
-        </textarea>
-      </div>
       <div className="w-1/3">
         <div className="grid gap-6 mb-6 md:grid-cols-2">
           <div>
@@ -123,9 +85,10 @@ const Karte = () => {
             />
           </div>
           <div>
-            <button className={`mt-7 text-white bg-${isConnected ? "gray" : "blue"}-700 hover:bg-${isConnected ? "gray" : "blue"}-800 focus:ring-4 focus:outline-none focus:ring-${isConnected ? "gray" : "blue"}-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-${isConnected ? "gray" : "blue"}-600 dark:hover:bg-${isConnected ? "gray" : "blue"}-700 dark:focus:ring-${isConnected ? "gray" : "blue"}-800`}
+            <button className={`mt-7 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-50`}
                     onClick={isConnected ? leaveRoom : joinRoom }
                     type="button"
+                    disabled={!userId}
             >
               { isConnected ? '退室' : '入室'}
             </button>
@@ -138,16 +101,9 @@ const Karte = () => {
           <input type="text" id="message"
                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                  value={input}
-                 onChange={(e) => setInput(e.target.value)}
+                 onChange={handleInputChange}
           />
         </div>
-        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                onClick={handleSend}
-                type="button"
-                disabled={!isConnected}
-        >
-          Submit
-        </button>
       </div>
     </>
   )
--- crdt-server/server.go
+++ crdt-server/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -46,150 +45,8 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/project/:id/karte", karteHandler)
+	e.GET("/project/karte/:id", karteHandler)
 	e.Logger.Fatal(e.Start(":8080"))
-}
-
-// ReceiveMessage represents a message sent to or from a client.
-type ReceiveMessage struct {
-	Key     string `json:"key"`
-	Content string `json:"content"`
-	SentBy  string `json:"sentBy"`
-}
-
-// SendMessage represents a message sent to or from a client.
-type SendMessage struct {
-	Key       string   `json:"key"`
-	Content   string   `json:"content"`
-	SentBy    string   `json:"sentBy"`
-	ProjectId string   `json:"projectId"`
-	UserIds   []string `json:"userIds"`
-}
-
-// joinRoom 入室
-func joinRoom(roomName string, clientID string) error {
-	roomKey := fmt.Sprintf("%s-room", roomName)
-	_, err := rdb.SAdd(ctx, roomKey, clientID).Result()
-	return err
-}
-
-// leaveRoom 退室
-func leaveRoom(roomName string, clientID string) error {
-	roomKey := fmt.Sprintf("%s-room", roomName)
-	_, err := rdb.SRem(ctx, roomKey, clientID).Result()
-	return err
-}
-
-// getUsersInRoom ルームにいるユーザー一覧を取得
-func getUsersInRoom(roomName string) ([]string, error) {
-	roomKey := fmt.Sprintf("%s-room", roomName)
-	return rdb.SMembers(ctx, roomKey).Result()
-}
-
-// cleanRoom ルームにいるユーザーがいなくなったらデータを削除
-func cleanRoom(roomName string) error {
-	userIds, err := getUsersInRoom(roomName)
-	if err != nil {
-		return err
-	}
-	if len(userIds) == 0 {
-		roomKey := fmt.Sprintf("%s-room", roomName)
-		key := "message"
-		dataKey := fmt.Sprintf("%s-data-%s", roomName, key)
-		// ここでDBに保存する
-		_, err = rdb.Del(ctx, roomKey, dataKey).Result()
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// readRoomData ルームのデータを読み込む
-func recoverRoomData(roomName string) ([]byte, error) {
-	// DBからRedisへのデータ移行
-	key := "message"
-	record := currentRecords[key]
-	dataKey := fmt.Sprintf("%s-data-%s", roomName, key)
-
-	err := rdb.Watch(ctx, func(tx *redis.Tx) error {
-		// Check if the key exists
-		n, err := tx.Exists(ctx, dataKey).Result()
-		if err != nil {
-			return err
-		}
-
-		// If the key doesn't exist, add the value
-		if n == 0 {
-			for _, o := range record {
-				data, err := json.Marshal(o)
-				if err != nil {
-					return err
-				}
-				_, err = tx.Pipelined(ctx, func(pipe redis.Pipeliner) error {
-					pipe.RPush(ctx, dataKey, string(data))
-					return nil
-				})
-				return err
-			}
-		}
-		return nil
-	}, dataKey)
-
-	if err != nil {
-		return []byte{}, err
-	}
-
-	// 最新のデータを取得
-	values, err := rdb.LRange(ctx, dataKey, 0, -1).Result()
-	if err != nil {
-		return []byte{}, err
-	}
-
-	userIDs, err := getUsersInRoom(roomName)
-	if err != nil {
-		return []byte{}, err
-	}
-
-	messages := make([]SendMessage, 0)
-	for _, v := range values {
-		var currentData O
-		err = json.Unmarshal([]byte(v), &currentData)
-		if err != nil {
-			return []byte{}, err
-		}
-
-		messages = append(messages, SendMessage{
-			Key:       key,
-			Content:   currentData.Value,
-			SentBy:    currentData.UpdatedBy,
-			ProjectId: roomName,
-			UserIds:   userIDs,
-		})
-	}
-
-	return json.Marshal(messages)
-}
-
-// rPush データを追加
-func rPush(roomName string, message ReceiveMessage) error {
-	key := message.Key
-	value := O{
-		Value:     message.Content,
-		UpdatedBy: message.SentBy,
-		UpdateAt:  time.Now(),
-	}
-	data, err := json.Marshal(value)
-	if err != nil {
-		return err
-	}
-
-	dataKey := fmt.Sprintf("%s-data-%s", roomName, key)
-	_, err = rdb.RPush(ctx, dataKey, data).Result()
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 // 仮データ
@@ -207,14 +64,9 @@ type O struct {
 
 func karteHandler(c echo.Context) error {
 	id := c.Param("id") // RoomNameとして利用
-	userID := c.QueryParam("userId")
 
 	if id == "" {
 		return echo.NewHTTPError(http.StatusBadRequest, "invalid room id")
-	}
-
-	if userID == "" {
-		return echo.NewHTTPError(http.StatusBadRequest, "invalid user id")
 	}
 
 	// WebSocketの接続を確立
@@ -229,18 +81,6 @@ func karteHandler(c echo.Context) error {
 		}
 	}()
 
-	// 入室
-	err = joinRoom(id, userID)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("failed to join room: %v", err))
-	}
-
-	data, err := recoverRoomData(id)
-	err = ws.WriteMessage(websocket.BinaryMessage, data)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("failed to write message: %v", err))
-	}
-
 	// サブスクライブの開始
 	pubsub := rdb.Subscribe(ctx, id)
 	defer func() {
@@ -254,33 +94,7 @@ func karteHandler(c echo.Context) error {
 	go func() {
 		ch := pubsub.Channel()
 		for sub := range ch {
-			userIDs, err := getUsersInRoom(id)
-			if err != nil {
-				log.Println("Failed to encode data:", err)
-				continue
-			}
-
-			var receiveMsg ReceiveMessage
-			err = json.Unmarshal([]byte(sub.Payload), &receiveMsg)
-			if err != nil {
-				log.Println("Failed to encode data:", err)
-				continue
-			}
-
-			messages := []SendMessage{{
-				Key:       receiveMsg.Key,
-				Content:   receiveMsg.Content,
-				SentBy:    receiveMsg.SentBy,
-				ProjectId: id,
-				UserIds:   userIDs,
-			}}
-
-			data, err := json.Marshal(messages)
-			if err != nil {
-				log.Println("Failed to encode data:", err)
-				continue
-			}
-			err = ws.WriteMessage(websocket.BinaryMessage, data)
+			err = ws.WriteMessage(websocket.BinaryMessage, []byte(sub.Payload))
 			if err != nil {
 				log.Printf("Error while writing message: %v", err)
 			}
@@ -295,19 +109,6 @@ func karteHandler(c echo.Context) error {
 			break
 		}
 
-		var receiveMsg ReceiveMessage
-		err = json.Unmarshal(message, &receiveMsg)
-		if err != nil {
-			log.Println("Failed to encode data:", err)
-			continue
-		}
-
-		err = rPush(id, receiveMsg)
-		if err != nil {
-			log.Println("Failed to encode data:", err)
-			continue
-		}
-
 		_, err = rdb.Publish(ctx, id, message).Result()
 		if err != nil {
 			log.Println("Failed to decode:", err)
@@ -315,16 +116,5 @@ func karteHandler(c echo.Context) error {
 		}
 	}
 
-	// 退室
-	err = leaveRoom(id, userID)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("failed to leave room: %v", err))
-	}
-
-	// 部屋にだれもいなければ片付ける
-	err = cleanRoom(id)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("failed to clean room: %v", err))
-	}
 	return nil
 }
